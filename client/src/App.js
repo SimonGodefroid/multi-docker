160d69f7415019cbe74d93d75368fcbce00e3fc0
SimonGodefroid <simon.godefroid@gmail.com>
2019-05-07 07:51:55 +0200

    Changed title

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
+            <h1>Fib Calculator</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
